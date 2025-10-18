560c024dcc81f630245219c112f84b1865958f8b
cylcoder <cylcoder@gmail.com>
2025-10-18 22:08:42 +0900

    Section 2: 프로젝트 환경설정

--- src/test/java/com/example/querydsl/QuerydslApplicationTests.java
+++ src/test/java/com/example/querydsl/QuerydslApplicationTests.java
@@ -1,10 +1,35 @@
 package com.example.querydsl;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
+@Commit
 class QuerydslApplicationTests {
+
+  @PersistenceContext
+  EntityManager entityManager;
+
+  @Test
+  void save() {
+    Demo demo = new Demo();
+    entityManager.persist(demo);
+    JPAQueryFactory queryFactory = new JPAQueryFactory(entityManager);
+    QDemo qDemo = QDemo.demo;
+    Demo foundDemo = queryFactory
+        .selectFrom(qDemo)
+        .fetchOne();
+
+    assertThat(foundDemo).isEqualTo(demo);
+  }
 
   @Test
   void contextLoads() {
